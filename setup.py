53c3422b7e81de9a2204c4210111e7d68b4fbe09
Carolina de Carvalho Nunes <carolina.decarvalhonunes@ugent.be>
2021-09-07 18:32:16 +0200

    license update

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ setup(
   name = 'HTSplotter',         # How you named your package folder (MyLib)
   packages = ['HTSplotter'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
-  license='GPL-3.0-only',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license='GNU General Public License v3 or later (GPLv3+)',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'An end-to-end data processing, '
                 'analysis and visualisation tool for chemical and genetic in '
                 'vitro perturbation screens',   # Give a short description about your library
-  author = 'Carolina Nunes',                   # Type in your name
+  author = 'CarolinadCNunes',                   # Type in your name
   author_email = 'carolina.decarvalhonunes@ugent.be',      # Type in your E-Mail
   url = 'https://github.com/CBIGR/HTSplotter',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/CBIGR/HTSplotter/archive/refs/tags/HTSplotter_01.tar.gz',    # I explain this later on
@@ -40,7 +40,7 @@ setup(
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GPL-3.0-only',   # Again, pick a license
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',   # Again, pick a license
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
