f941a67d06f82fc37673cf91065353ed8313d54e
jaanckae <31304271+jaanckae@users.noreply.github.com>
2023-06-07 15:42:35 +0200

    Update submit_analysis.php

--- HTSplotter/web/submit_analysis.php
+++ HTSplotter/web/submit_analysis.php
@@ -4,11 +4,12 @@ include_once('functions.php');
 if ($_POST['user_input'] == 0){
 	$date_part = date("Ymd");
 	$user_string = generateRandomString();
-	$user_dir = 'user_files/User_'.$user_string.'_'.$date_part.'_HTSplotter/';
-	$response_array['user_input'] = $user_dir;
+	$cmd_dir = 'user_files/User_'.$user_string.'_'.$date_part.'_HTSplotter/';
+	$user_dir = $cmd_dir.'Inputfile/';
+	$response_array['user_input'] = $cmd_dir;
 
 	$oldumask = umask(0); 
-	mkdir($user_dir, 0777);
+	mkdir($user_dir, 0777, TRUE);
 	umask($oldumask);
 
 	$total = count($_FILES['csv_file']['name']);
@@ -19,11 +20,11 @@ if ($_POST['user_input'] == 0){
 	}
 
 
-	$command = 'python3 /var/www/html/scripts/web.py -f /var/www/html/'.$user_dir.' -b '.escapeshellarg($_POST['bio_rep']).' -n '.escapeshellarg($_POST['biorep_fn']).' -e '.escapeshellarg($_POST['exp_effect']).' -i '.escapeshellarg($_POST['info_readout']).' -r '.escapeshellarg($_POST['readout_unit']).' -u '.escapeshellarg('no').' -sh '.escapeshellarg($_ENV['S3_HOST']).' -sb '.escapeshellarg($_ENV['S3_BUCKET']).' -sa '.escapeshellarg($_ENV['S3_ACCESS_KEY']).' -ss '.escapeshellarg($_ENV['S3_SECRET_KEY']);
+	$command = 'python3 /var/www/html/scripts/web.py -f /var/www/html/'.$cmd_dir.' -b '.escapeshellarg($_POST['bio_rep']).' -n '.escapeshellarg($_POST['biorep_fn']).' -e '.escapeshellarg($_POST['exp_effect']).' -i '.escapeshellarg($_POST['info_readout']).' -r '.escapeshellarg($_POST['readout_unit']).' -sy '.escapeshellarg($_POST['syn_ana']).' -u '.escapeshellarg('no').' -sh '.escapeshellarg($_ENV['S3_HOST']).' -sb '.escapeshellarg($_ENV['S3_BUCKET']).' -sa '.escapeshellarg($_ENV['S3_ACCESS_KEY']).' -ss '.escapeshellarg($_ENV['S3_SECRET_KEY']);
 	$output = shell_exec($command);
 
 	$errorfile = '';
-	$error_array = glob($user_dir.'results/*_Errorfile.txt');
+	$error_array = glob($user_dir.'*_Errorfile.txt');
 	if (!empty($error_array)) {
 		$errorfile = $error_array[0];
 	}
@@ -45,7 +46,7 @@ if ($_POST['user_input'] == 0){
 			$filename_str = $filename_str.trim(explode(": ", $filenames[$key])[1]).",";
 			$name_str1 = $name_str.trim(explode(": ", $name[$key])[1]).",";
 			$name_str2 = explode(".", $name_str1);
-			$infofile = $infofile.trim($user_dir.'results/'.$name_str2[0].'_information.txt').",";
+			$infofile = $infofile.trim($cmd_dir.$name_str2[0].'_information.txt').",";
 		}
 		$response_array["exp_types"] = substr($exptype_str, 0, -1);
 		$response_array["filenames"] = substr($filename_str, 0, -1);
@@ -54,14 +55,17 @@ if ($_POST['user_input'] == 0){
 	echo json_encode($response_array);
 } else {
 	$exptype_str = "'".str_replace(",","' '",$_POST['exp_types'])."'";
-	$command = 'python3 /var/www/html/scripts/web.py -f /var/www/html/'.$_POST['user_input'].' -b '.escapeshellarg($_POST['bio_rep']).' -n '.escapeshellarg($_POST['biorep_fn']).' -e '.escapeshellarg($_POST['exp_effect']).' -i '.escapeshellarg($_POST['info_readout']).' -r '.escapeshellarg($_POST['readout_unit']).' -u '.escapeshellarg('yes').' -t '.$exptype_str.' -sh '.escapeshellarg($_ENV['S3_HOST']).' -sb '.escapeshellarg($_ENV['S3_BUCKET']).' -sa '.escapeshellarg($_ENV['S3_ACCESS_KEY']).' -ss '.escapeshellarg($_ENV['S3_SECRET_KEY']);
+	$command = 'python3 /var/www/html/scripts/web.py -f /var/www/html/'.$_POST['user_input'].' -b '.escapeshellarg($_POST['bio_rep']).' -n '.escapeshellarg($_POST['biorep_fn']).' -e '.escapeshellarg($_POST['exp_effect']).' -i '.escapeshellarg($_POST['info_readout']).' -r '.escapeshellarg($_POST['readout_unit']).' -sy '.escapeshellarg($_POST['syn_ana']).' -u '.escapeshellarg('yes').' -t '.$exptype_str.' -sh '.escapeshellarg($_ENV['S3_HOST']).' -sb '.escapeshellarg($_ENV['S3_BUCKET']).' -sa '.escapeshellarg($_ENV['S3_ACCESS_KEY']).' -ss '.escapeshellarg($_ENV['S3_SECRET_KEY']);
 	$output = shell_exec($command);
 	$oparray = preg_split("#[\r\n]+#", trim($output));
 	$result = preg_grep("/^Output/i", $oparray);
 	$result = array_values($result);
-	$response_array["pdf"] = trim(explode(": ", $result[0])[1]);
+	$response_array['pdf'] = trim(explode(": ", $result[0])[1]);
+	$response_array['html'] = str_replace('/var/www/html/', '', glob('/var/www/html/'.$_POST['user_input'].'Output_html/' . "{*.html}", GLOB_BRACE));
 	$response_array['user_input'] = $_POST['user_input'];
 	$response_array['zip'] = 'https://'.$_ENV['S3_HOST'].'/'.$_ENV['S3_BUCKET'].'/'.explode("/", $_POST['user_input'])[1].'.zip';
+	//$response_array['zip'] = 'https://'.$_ENV['S3_HOST'].'/'.$_ENV['S3_BUCKET'].'/'.explode("/", $_POST['user_input'])[1].'.zip';
+
 	echo json_encode($response_array);
 }
 
